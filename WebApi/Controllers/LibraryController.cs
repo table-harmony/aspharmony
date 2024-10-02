69ab9d2e7a59fe6beeffc49013fa15b6ecf7b790
table-harmony <lironkaner2007@gmail.com>
2024-10-02 21:42:03 +0300

    web api

--- WebApi/Controllers/LibraryController.cs
+++ WebApi/Controllers/LibraryController.cs
@@ -1,46 +1,61 @@
-﻿using BusinessLogicLayer.Services;
+﻿using Microsoft.AspNetCore.Mvc;
+using BusinessLogicLayer.Services;
 using DataAccessLayer.Entities;
-using Microsoft.AspNetCore.Mvc;
 
-namespace Api.Controllers {
+namespace PresentationLayer.Controllers {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class LibraryController : ControllerBase {
-
-        private readonly ILogger<LibraryController> _logger;
         private readonly ILibraryService _libraryService;
 
-        public LibraryController(ILogger<LibraryController> logger,
-                                    ILibraryService libraryService) {
-            _logger = logger;
+        public LibraryController(ILibraryService libraryService) {
             _libraryService = libraryService;
         }
 
-        [HttpGet(Name = "GetLibraries")]
-        public async Task<IEnumerable<Library>> Get() {
-            return await _libraryService.GetAllAsync();
+        // GET: api/Library
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Library>>> GetAllLibraries() {
+            var libraries = await _libraryService.GetAllAsync();
+            return Ok(libraries);
         }
 
-        [HttpGet(Name = "GetLibrary")]
-        public async Task<Library> Get(int id) {
-            return await _libraryService.GetLibraryAsync(id);
+        // GET: api/Library/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Library>> GetLibrary(int id) {
+            var library = await _libraryService.GetLibraryAsync(id);
+            if (library == null) {
+                return NotFound();
+            }
+            return Ok(library);
         }
 
-        [HttpPost(Name = "CreateLibrary")]
-        public async void Post(string name) {
-            Library library = new() { Name = name };
+        // POST: api/Library
+        [HttpPost]
+        public async Task<ActionResult<Library>> CreateLibrary(Library library) {
             await _libraryService.CreateAsync(library);
+            return CreatedAtAction(nameof(GetLibrary), new { id = library.Id }, library);
         }
 
-        [HttpDelete(Name = "DeleteLibrary")]
-        public async void Delete(int id) {
-            await _libraryService.DeleteAsync(id);
+        // PUT: api/Library/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateLibrary(int id, string name) {
+            try {
+                await _libraryService.UpdateAsync(id, name);
+            } catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+            return NoContent();
         }
 
-        [HttpPatch(Name = "UpdateLibrary")]
-        public async void Patch(int id, string name) {
-            await _libraryService.UpdateAsync(id, name);
+        // DELETE: api/Library/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLibrary(int id) {
+            try {
+                await _libraryService.DeleteAsync(id);
+            } catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+            return NoContent();
         }
-
     }
 }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -2,34 +2,32 @@ using BookServiceReference;
 using BusinessLogicLayer.Events;
 using BusinessLogicLayer.Services;
 using DataAccessLayer.Data;
-using DataAccessLayer.Entities;
 using DataAccessLayer.Repositories;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationContext>(options =>
-  options.UseSqlServer("Server=(LocalDB)\\MSSQLLocalDB;Database=AspHarmonyDB;Trusted_Connection=True;MultipleActiveResultSets=true")
-    .EnableSensitiveDataLogging());
-
-// Register Identity services
-builder.Services.AddIdentity<User, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationContext>()
-    .AddDefaultTokenProviders();
-
-// Register the SOAP client
-builder.Services.AddScoped<BookServiceSoapClient>(_ =>
-    new BookServiceSoapClient(BookServiceSoapClient.EndpointConfiguration.BookServiceSoap));
-
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationContext>(options =>
+              options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+                .EnableSensitiveDataLogging());
+
+builder.Services.AddScoped<BookServiceSoapClient>(_ =>
+                new BookServiceSoapClient(BookServiceSoapClient.EndpointConfiguration.BookServiceSoap));
+
+// Register your repositories
 builder.Services.AddScoped<ILibraryRepository, LibraryRepository>();
 builder.Services.AddScoped<ILibraryMembershipRepository, LibraryMembershipRepository>();
 builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<ILibraryBookRepository, LibraryBookRepository>();
 builder.Services.AddScoped<IBookLoanRepository, BookLoanRepository>();
+
+// Register your services
 builder.Services.AddScoped<ILibraryService, LibraryService>();
 builder.Services.AddScoped<ILibraryMembershipService, LibraryMembershipService>();
 builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<ILibraryBookService, LibraryBookService>();
+builder.Services.AddScoped<IBookLoanService, BookLoanService>();
 builder.Services.AddScoped<IEventPublisher, EventPublisher>();
 
 builder.Services.AddControllers();
